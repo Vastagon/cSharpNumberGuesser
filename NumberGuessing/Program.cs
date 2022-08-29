07f59eeb00955c78b8cd91835d80523bb5962d2f
Jacob Yancey <jyancey@centretechnologies.com>
2022-08-29 12:20:26 -0500

    somewhat pointless method calls for practice

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+
+namespace numberGuessing
+{
+    class guessNumber
+    {
+
+
+        public static void Main(String[] args)
+        {
+            Random random = new Random();
+            int target = random.Next(0, 1000);
+            Console.WriteLine("Guess a number between 1 and 1000");
+
+            while (true)
+            {
+                int guessedNumber = int.Parse(Console.ReadLine());
+                if(guessNumber.checkTarget(guessedNumber, target))
+                {
+                    Console.WriteLine("You guessed correctly");
+                    return;
+                }
+            }
+        }
+
+        public static Boolean checkTarget(int guessedNumber, int target)
+        {
+            if (target > guessedNumber)
+            {
+                Console.WriteLine("Too low, try again");
+                return false;
+            }
+            if (target < guessedNumber)
+            {
+                Console.WriteLine("Too high, try again");
+                return false;
+            }
+            return true;
+        }
+    }
+}
